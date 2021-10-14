5b964b5426c27c8ae5b242f7261dd91fd984132c
Sricharan Ramesh <charan1952001@gmail.com>
2021-10-14 19:24:07 +0530

    feat: add react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg'
 import './App.css'
+import { BrowserRouter, Route, Redirect } from 'react-router-dom'
+import Home from './Pages/Home'
+import Landing from './Pages/Landing'
 
 function App () {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/home' component={Home} />
+        <Redirect to='/' />
+      </BrowserRouter>
     </div>
   )
 }
