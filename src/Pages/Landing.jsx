bd1269bb44e75cb16cc816de82547749fb14f4c3
Sricharan Ramesh <charan1952001@gmail.com>
2021-10-16 21:50:45 +0530

    feat: add landing page

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div className='h-screen bg-indigo-900 pt-24'>
-      <h1>Landing</h1>
+    <div className='h-screen pt-24 px-16'>
+      <div className='text-4xl md:text-left font-sora'>QR Child Tracker</div>
+      <p className='mt-10 md:text-left md:text-lg'>A way to keep track of your child</p>
     </div>
   )
 }
