9d00cdbff200cbf1d169ae2856f4ada39397a574
Sricharan Ramesh <charan1952001@gmail.com>
2021-12-02 09:02:39 +0530

    fix: minor fixes

--- src/Pages/QRGenerator.jsx
+++ src/Pages/QRGenerator.jsx
@@ -10,12 +10,11 @@ const QRGenerator = () => {
     <div className='h-screen w-screen p-10'>
       <div className='flex items-center justify-evenly h-full w-full'>
         <div className='flex flex-col items-start gap-6 font-sora'>
-          <img src={shirt} alt="shirt" />
+          <img src={shirt} alt="shirt" className='w-96' />
           <p className='text-2xl'>Your Unique QR Code</p>
           <p>This QR code will be embedded in your clothes</p>
           <div className='flex gap-4'>
             <button className='bg-newblue hover:bg-newdarkblue px-8 py-3 text-lg text-white rounded-md'>Shop now</button>
-            <button className='bg-newblue hover:bg-newdarkblue px-8 py-3 text-lg text-white rounded-md'>Update Details</button>
           </div>
         </div>
         <QRCode
