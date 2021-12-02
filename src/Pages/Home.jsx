73c57552ee48e840750a73d54282e8110f99f5ea
Sricharan Ramesh <charan1952001@gmail.com>
2021-12-02 07:31:25 +0530

    fix: home illustration

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,9 +7,11 @@ const Home = () => {
       <div className='relative overflow-hidden pt-20 h-full'>
         <div className='pt-8 text-left pl-14 h-full'>
           <h1 className='text-3xl font-sora font-bold w-2/3 my-14 mx-4'>Let's get you started!</h1>
-          <p className='mt-5 mx-8 text-xl font-medium w-2/3'>1. Register point of contact details</p>
-          <p className='mt-5 mx-8 text-xl font-medium w-2/3'>2. Get your unique QR code</p>
+          <p className='mt-5 mx-8 text-xl font-medium w-2/3'>1. Register all of your point of contact details.</p>
+          <p className='mt-5 mx-8 text-xl font-medium w-2/3'>2. Get your unique QR code.</p>
           <p className='mt-5 mx-8 text-xl font-medium w-2/3'>3. Shop your favorite clothes with the QR code embedded!</p>
+          <p className='mt-5 mx-8 text-xl font-medium w-1/2'>4. If your child is lost, any public citizen can easily scan the QR code and details of the stranger will be sent on registered members’ phone.</p>
+          <p className='mt-5 mx-8 text-xl font-medium w-1/2'>5. You can always add a new member who will help you find your child more quickly.</p>
 
           <div className='flex flex-col items-center w-1/3 pt-8 ml-10'>
             <a
@@ -20,7 +22,7 @@ const Home = () => {
             </a>
           </div>
         </div>
-        <img alt='desktop' className='absolute h-1/2 right-16 top-1/4' src={HomeSvg} />
+        <img alt='desktop' className='absolute h-2/3 right-24 top-1/4' src={HomeSvg} />
       </div>
     </div>
   )
